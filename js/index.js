02ab6be08eaef800ad45565b8b8b00915f2da023
WStudioMAIN <118752806+WStudioMAIN@users.noreply.github.com>
2022-11-22 22:45:40 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ document.getElementById("ie_2_2").style.display="none";
 function s_h_(){
     var s_h = document.getElementById("sidebarId").scrollHeight;
 	var s_w = document.getElementById("sidebarId").scrollWidth;
+	
 	document.getElementById("si").style.width = s_w + "px";
     document.getElementById("si").style.height = s_h + "px";
 	
@@ -35,6 +36,6 @@ function date(){
 	let week=oDate.getDay()+1;
 	//获取今天星期
 	var x = document.getElementById("Date");
-	x.innerHTML = "日期:" + y + "年" + M + "月" + day + "号" + h + "点" + m + "分" + s + "秒";
+	x.innerHTML = "Date time:" + y + "年" + M + "月" + day + "号" + h + "点" + m + "分" + s + "秒";
 }
 date();
