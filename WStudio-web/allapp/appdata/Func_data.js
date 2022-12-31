43451d3e0861c1ae3ff3bb76dbb584fbf5942070
WorldStudio <118752806+WorldStudio-com@users.noreply.github.com>
2022-12-31 15:15:00 +0800

    Add files via upload

---
+++ WStudio-web/allapp/appdata/Func_data.js
@@ -0,0 +1,35 @@
+function VBOX() {
+    document.getElementById('BOX').style.display = "none";
+}
+
+document.getElementById('appdata').style.display = "none";
+
+
+//
+function a() {
+
+    setTimeout(() => {
+        VBOX();
+        alert('gg');
+    }, 500);
+
+}
+
+
+
+
+
+
+
+
+
+
+//
+function b() {
+
+    setTimeout(() => {
+        VBOX();
+        alert('gg');
+    }, 500);
+
+}
---
+++ WStudio-web/js/index-WStudio.js
@@ -0,0 +1,44 @@
+//成员列表
+
+function ers(A, B, C, D) {
+  var list_0 =
+    '<div id="MeB"><p class="break">-</p><p class="break">-</p><div class="memberBox"><div><p class="break">-</p><img class="memberHeader" style=" position: relative;left: 3px; " src="';
+  //img/memberHeadr/1.png
+  var list_1 =
+    '"><div class="memberData"><div><p style=" position: relative;left: 6px; "><button style="font-size: 1.5em;" onclick=""+D+"">';
+  //名字
+  var list_2 =
+    '</button><hr style="line-height: 0.3em;position: relative;left: 6px;"></div><p style="font-size: 11px;position: relative;left: 6px;">';
+  //介绍
+  var list_3 =
+    '</p></div><p class="break">-</p></div></div><p class="break">-</p><p class="break">-</p></div>';
+
+  var m = list_0 + A + list_1 + B + list_2 + C + list_3;
+  return m;
+}
+
+function memberList() {
+  const temp = document.getElementById("memberList");
+  temp.innerHTML += ers("img/memberHeadr/1.png", "WStudio", "全栈工程师");
+}
+memberList();
+
+/*图片自动切换*/
+var imgAmount = 1;
+var imgMax = 2; //展示图片数量
+
+setInterval(() => {
+  var temp = document.getElementById("imgMain");
+  var path = "img/MainShow/" + imgAmount + ".png";
+  temp.src = path;
+  imgAmount += 1;
+  //alert(path);
+  if (imgAmount > imgMax) {
+    imgAmount = 1;
+  }
+}, 3 * 1000);
+
+//修改levelBox高
+//var levelBoxH = document.getElementById('levelBoxM');
+
+//document.getElementById("levelBox").style.height = levelBoxH.offsetHeight + 10 + "px";
