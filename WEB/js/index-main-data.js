9eada7e15f13d69ed368adaef31b9590cd70a265
WorldStudio <118752806+WorldStudio-com@users.noreply.github.com>
2023-01-08 23:49:44 +0800

    Add files via upload

---
+++ WEB/js/index-main-data.js
@@ -0,0 +1,107 @@
+
+var WorldStudio_Main = document.getElementById("WorldStudio_Main");
+
+function Official_Web_Home_Page_F() {
+    WorldStudio_Main.innerHTML =
+      '\
+      <div id="Official_Web_Home_Page">\
+        <div id="Official_Web_Home_Page_backimg"></div>\
+        <div id="Official_Web_Home_Page_tagBox">\
+          <button style="border-bottom-color: rgb(0, 225, 255);">WorldStudio 专业</button>\
+          <button id="Official_Web_Home_Page_ContactUs" onclick="WorldStudio_bottom_BBS();">💝联系WorldStudio</button>\
+        </div>\
+        <div id="Official_Web_Home_Page_Main">\
+          <div style="height: 80px;"></div>\
+          <div id="Official_Web_Home_Page_MainB">\
+            <p>\
+              <span>WorldStudio案例🚀</span>\
+            </p>\
+            <div>\
+              <img id="WorldStudioProjectCase" src="res/WorldStudioProjectCase_0.png" alt="案例" draggable="false">\
+            </div>\
+            <div>\
+              <p style="margin-top: 50px;">\
+                <span>WorldStudio🚀</span>\
+              </p>\
+              <br>\
+              <div class="k_s_j">\
+                <div>\
+                  <span style="font-size: 1.3em;">开发</span>\
+                  <span>静态网页</span>\
+                  <span>跨平台应用</span>\
+                </div>\
+                <div>\
+                  <span style="font-size: 1.3em;">设计</span>\
+                  <span>平面设计</span>\
+                  <span>框架设计</span>\
+                </div>\
+                <div>\
+                  <span style="font-size: 1.38em;">技术</span>\
+                  <span>C++</span>\
+                  <span>C</span>\
+                  <span>QT</span>\
+                  <span>JS</span>\
+                  <span>CSS</span>\
+                  <span>HTML</span>\
+                </div>\
+              </div>\
+            </div>\
+\
+            <div>\
+              <p style="margin-top: 50px;">\
+                <span>WorldStudio成员🚀</span>\
+              </p>\
+              <br>\
+              <div class="WorldStudioMember">\
+                <p style="height: 5px;"></p>\
+                <img src="res/WS.png">\
+                <div>\
+                  <span style="border-bottom-style: solid;">王佳明</span>\
+                  <br>\
+                  <p style="height: 5px;"></p>\
+                  <span style="padding-left: 1.5em;">不知道写什么介绍.</span>\
+                </div>\
+              </div>\
+            </div>\
+\
+            <div>\
+              <p style="margin-top: 50px;">\
+                <span>关于WorldStudio🚀</span>\
+              </p>\
+              <br>\
+              <div style="background-color: rgb(214, 214, 214);width: 90%;position: relative;left: 5%;">\
+                <div style="display: inline-block;vertical-align: top;height: 100px;"><p style="word-break:break-all;">===============================================</p></div>\
+              </div>\
+            </div>\
+\
+            <div>\
+              <p style="margin-top: 50px;">\
+                <span>WorldStudio新闻🚀</span>\
+              </p>\
+              <br>\
+              <div class="WorldStudioNewT">\
+                <p style="height: 5px;"></p>\
+                <img src="res/WS.png">\
+                <div>\
+                  <button style="border-bottom-style: solid;">WorldStudio</button>\
+                  <span style="padding-left: 1.5em;">💝</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+          <div style="height: 50px;"></div>\
+        </div>\
+        <div id="Official_Web_Home_Page_caihong">\
+          <div style="background-color: rgb(255, 0, 0);">💝 Hello! 💝</div>\
+          <div style="background-color: rgb(255, 153, 0);">💝 Hello! 💝</div>\
+          <div style="background-color: rgb(246, 255, 0);">💝 Hello! 💝</div>\
+          <div style="background-color: rgb(0, 255, 4);">💝 Hello! 💝</div>\
+          <div style="background-color: rgb(0, 255, 225);">💝 Hello! 💝</div>\
+          <div style="background-color: rgb(0, 51, 255);">💝 Hello! 💝</div>\
+          <div style="background-color: rgb(255, 0, 242);">💝 Hello! 💝</div>\
+        </div>\
+      </div>\
+      \
+    </div>\
+    ';
+}
