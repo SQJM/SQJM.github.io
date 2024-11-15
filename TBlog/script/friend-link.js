2629c8e55ef861c52fdf20908e70d00f14fa6c72
This Blog <hello@>
2024-11-15 22:10:56 +0800

    Delete item: TBlog/script/friend-link.js

--- TBlog/script/friend-link.js
+++
@@ -1,1 +0,0 @@
- ThisBlog_Data["friend-link"] = {"links":[{"name":"fwf","link":"wff","introduce":"fwf"},{"name":"gewg","link":"gwaerg","introduce":"gewg"},{"name":"rr","link":"rr","introduce":"tt"}]};
