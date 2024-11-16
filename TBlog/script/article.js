81a2fba7d6e1b9701100f3aaa7eb38bbb6cd7b47
This Blog <hello@>
2024-11-17 00:54:01 +0800

    Upload article.js file

---
+++ TBlog/script/article.js
@@ -0,0 +1,85 @@
+/**
+ * @name ThisBlog Article Js File
+ * @description 文章引入文件
+ * @author Wang Jia Ming
+ * @createDate 2024-11-01
+ */
+'use strict';
+
+function send( data = {} ) {
+	const json = `{"ARTICLE":${ JSON.stringify( data ) }}`;
+	window.parent.postMessage( JSON.stringify( JSON.parse( json ) ), '*' );
+}
+
+(() => {
+	const style = document.createElement( "style" );
+	style.id = "article-style";
+	style.setAttribute( "type", "text/css" );
+	style.textContent = `\
+html {
+
+}
+
+body {
+   opacity: 0;
+   animation: start 0.5s forwards;
+}
+
+img {
+   width: 100%;
+}
+
+@keyframes start {
+    to {
+        opacity: 1;
+    }
+}
+
+.article-info {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+}
+
+.article-content {
+    margin-top: 20px;
+    margin-bottom: 35px;
+    border-radius: 6px;
+    background-color: #ebebeb;
+    padding: 10px;
+}
+
+.article-tags {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+}
+
+.article-tags > .item {
+    text-wrap: nowrap;
+    border-radius: 6px;
+    background-color: #ebebeb;
+    padding: 3px 6px 3px 6px;
+}`;
+	document.body.appendChild( style );
+})();
+
+window.addEventListener( "load", () => {
+	send( {
+		height: document.body.getClientRects().item( 0 ).height
+	} );
+	document.body.style.animation = null;
+} );
+
+document.addEventListener( "load", () => {
+	console.log( document.body.getClientRects().item( 0 ).height )
+	send( {
+		height: document.body.getClientRects().item( 0 ).height
+	} );
+	document.body.style.animation = null;
+} );
+
+window.addEventListener( "message", ( event ) => {
+	const data = event.data;
+	console.log( data )
+} );
