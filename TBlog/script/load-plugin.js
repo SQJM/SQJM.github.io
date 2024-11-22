20c80673d9aed730ac4405f6ee20c48e17040670
This Blog <hello@>
2024-11-22 11:30:13 +0800

    Upload load-plugin.js file

---
+++ TBlog/script/load-plugin.js
@@ -0,0 +1,1 @@
+( () => { ["./script/plugin_Easy.js"].forEach( v => { const script = document.createElement( "script" ); script.type = "text/javascript"; script.src = v; document.body.appendChild( script ); } ); } )();
