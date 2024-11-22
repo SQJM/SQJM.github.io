c0fe5059629a9cdf218510384bfcc4e33d7af308
This Blog <hello@>
2024-11-22 21:19:46 +0800

    Upload friend-link.js file

---
+++ TBlog/script/friend-link.js
@@ -0,0 +1,1 @@
+ ThisBlog_Data["friend-link"] = {"links":[{"name":"fwf","link":"wff","introduce":"fwf"},{"name":"gewg","link":"gwaerg","introduce":"gewg"},{"name":"rr","link":"rr","introduce":"tt"},{"name":"a","link":"a","introduce":"a"},{"name":"s","link":"s","introduce":"s"}]};
